aef982ab6a51f19e9881a31de4e477c3d4d174cc
NanoCode012 <kevinvong@rocketmail.com>
2017-09-12 14:19:58 +0700

    Added new section

---
+++ CSharpBasic_DateTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasic_DateTime
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
