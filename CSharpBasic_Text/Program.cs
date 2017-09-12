77a9895fb15f794db121e0d94845da35a7f59c75
NanoCode012 <kevinvong@rocketmail.com>
2017-09-12 22:18:49 +0700

    * StringSummarizer.cs: * CSharpBasic_Text.csproj: Moved method to new class for re-usability
    * Program.cs: Added info on strings.

--- CSharpBasic_Text/Program.cs
+++ CSharpBasic_Text/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CSharpBasic_Text
 {
@@ -6,7 +8,42 @@ namespace CSharpBasic_Text
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string methods
+            var myName = " Tom Ladder ";
+            Console.WriteLine("Trim '{0}' ", myName.Trim());
+            Console.WriteLine("ToUpper '{0}' ", myName.ToUpper().Trim());
+            Console.WriteLine("ToLower '{0}' ", myName.ToLower().Trim());
+            var arrayName = myName.Trim().Split(' ');
+            Console.WriteLine("ArrayName : {0},{1}", arrayName[0], arrayName[1]);
+            var index = myName.Trim().IndexOf(' ');
+            var firstName = myName.Trim().Substring(0, index);
+            var lastName = myName.Trim().Substring(index + 1);
+            Console.WriteLine("f,l {0},{1}", firstName, lastName);
+            var fullName = myName.Replace("Tom", "Tomonit");
+            var replacedName = myName.Replace('o', '0');
+            Console.WriteLine("fullName : " + fullName);
+            Console.WriteLine("replacedName : " + replacedName);
+            Console.WriteLine("Pls enter creditCard:\t\t\t\t(PS:We won't log it)");
+            var creditCard = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(creditCard))
+            {
+                Console.WriteLine("Invalid card!");
+            }
+            //Convert int to string and vice-versa
+            var input = "30";
+            byte input2 = byte.Parse(input);
+            byte input3 = Convert.ToByte(input);
+            Console.WriteLine(input2 + " | " + input3);
+            var price = 29.98f;
+            var priceStr = Convert.ToString(price);
+            var priceStr1 = price.ToString("C");
+            Console.WriteLine(priceStr + " | " + priceStr1);
+
+            //Summarizing text
+            var text = "This is going to be a super really omega(ly) freakishly" +
+                " amazingly perpetually impossiblely the longest sentence, NOT.";
+            var summarizedText = StringSummarizer.Summarize(text);
+            Console.WriteLine("summarizedText : " + summarizedText);
         }
     }
 }
---
+++ CSharpBasic_Text/StringSummarizer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpBasic_Text
+{
+    public class StringSummarizer
+    {
+		public static string Summarize(string text, int maxLength = 50)
+		{
+			//method1
+			//var lastSpace = 0;
+			var show_text = String.Empty;
+			if (text.Length > maxLength)
+			{
+				if (text[maxLength] == ' ')
+				{
+					show_text = text.Substring(0, maxLength);
+					//Console.WriteLine("ShowText : " + show_text + "...");
+				}
+				else
+				{
+					//This works too. It is basically the same as LastIndexOf
+					//This code if code block can be shortened, but kept for
+					//self others to understand the code
+					//method1_0
+					/*for (var i = 0; i < maxLength; i++)
+					{
+						if(text[i] == ' ')
+						{
+							lastSpace = i;
+						}
+					}*/
+					//var show_text = text.Substring(0, lastSpace);
+					//method1_2
+					show_text = text.Substring(0, maxLength);
+					show_text = text.Substring(0, show_text.LastIndexOf(' '));
+					//Console.WriteLine("show_text : " + show_text + "...");
+				}
+			}
+			else
+			{
+				show_text = text;
+				Console.WriteLine("show_text : " + show_text);
+			}
+			//method2
+			//A harsher way to do the method above
+			if (text.Length > maxLength)
+			{
+				var words = text.Split(' ');
+				var sumCharacter = 0;
+				var listWords = new List<string>();
+				foreach (var word in words)
+				{
+					sumCharacter += word.Length + 1;//add 1 cuz of space after word
+					if (sumCharacter >= maxLength)
+					{
+						Console.WriteLine("word is where is cuts : " + word);
+						break;
+					}
+					listWords.Add(word);
+				}
+				show_text = String.Join(" ", listWords) + "...";
+
+			}
+			else
+			{
+				Console.WriteLine("text : " + text);
+			}
+			return show_text;
+		}
+    }
+}
