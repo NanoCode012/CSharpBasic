7a1b99f3c9f9d31afe880053bee1fe894081e4d4
NanoCode012 <kevinvong@rocketmail.com>
2017-09-15 14:24:57 +0700

    Made it more readable. Improved comments.

--- CSharpBasic_PrimitivetypesExpression/Program.cs
+++ CSharpBasic_PrimitivetypesExpression/Program.cs
@@ -13,6 +13,7 @@ namespace CSharpBasic_PrimitivetypesExpression
             char Let = 'P';
             string Book = "Do me a favor \n ?";
             bool Happy = false;
+
             Console.WriteLine("Number :" +Number);
             Console.WriteLine("Hell :" +Hell);
             Console.WriteLine("Duck :" +Duck);
@@ -29,7 +30,9 @@ namespace CSharpBasic_PrimitivetypesExpression
             //Constants
             const float Pi = 3.14f;
             Console.WriteLine(FindArea(4));
-            float FindArea(float radius) //for some reason this works :P
+            float FindArea(float radius)//this works :P
+                                        //check enscapulation, this method only 
+                                        //allowed in Main() method
 			{
 				float a;
 				a = Pi * (radius * radius);
@@ -37,17 +40,19 @@ namespace CSharpBasic_PrimitivetypesExpression
 			}
 
             //Implicit type conversion
-            byte b = 1; //byte is 1 byte is 8 bit 
-            int i = b; //integer is 4 bytes is 32 bit hence Int32
-            Console.WriteLine("i :" +i);
+            byte b = 1; //1 byte = 8 bit 
+            int i = b; //integer = 4 bytes = 32 bit hence Int32
+            Console.WriteLine("i : " +i);//no data loss
+
             //Explicit type conversion using cast
             int i2 = 1000;
             byte b2 = (byte)i2;
-            Console.WriteLine("b2 :" +b2);
+            Console.WriteLine("b2 : " +b2);//has data loss, check bit conversion
+
             //Conversion between non-compatible types
             string s = "50";
             int i3 = Convert.ToInt32(s);
-            Console.WriteLine("i3 :" +i3);
+            Console.WriteLine("i3 : " +i3);
             string s2 = "true";
             bool B = Convert.ToBoolean(s2);
             Console.WriteLine(B);
@@ -58,35 +63,33 @@ namespace CSharpBasic_PrimitivetypesExpression
             {
 				string s3 = "1000";
 				byte b3 = Convert.ToByte(s3);
-				Console.WriteLine("b3 :" + b3);
+				Console.WriteLine("b3 : " + b3);
             }
             catch (Exception)
             {
                 Console.WriteLine(  "An error occured. The number didn't "
-                                  + "fit the given range"); //to fit screen
+                                  + "fit the given range"); //seperate to fit
             }
 
             //Logical Operators
             byte b4 = 12;
             byte b5 = 6;
             byte b6 = 18;
-            Console.WriteLine("b4 :" +b4);
-            Console.WriteLine("b5 :" + b5);
-            Console.WriteLine("b6 :" + b6);
-            Console.WriteLine("b4+b5 :" + (b4+b5));
-            Console.WriteLine("b4-b5 :" + (b4-b5));
-            Console.WriteLine("b4*b5 :" + (b4*b5));
-            Console.WriteLine("b4/b5 :" + (b4/b5));
-            Console.WriteLine("b4>b5 :" + (b4 > b5));
-            Console.WriteLine("b4==b5 :" + (b4 == b5));
-            Console.WriteLine("b4!=b5 :" + (b4 != b5 ));
-            Console.WriteLine("b6>b4 && b6>b5 :" + (b6 > b4 && b6 > b5));
-            Console.WriteLine("b6>b4 && b6==b5 :" + (b6 > b4 && b6 == b5));
-            Console.WriteLine("b6>b4 || b6==b5 :" + (b6 > b4 || b6 == b5));
-            Console.WriteLine("!(b6>b4 || b6==b5) :" + !(b6 > b4 || b6 == b5));
 
+            Console.WriteLine("b4 : " +b4);
+            Console.WriteLine("b5 : " + b5);
+            Console.WriteLine("b6 : " + b6);
+            Console.WriteLine("b4+b5 : " + (b4+b5));
+            Console.WriteLine("b4-b5 : " + (b4-b5));
+            Console.WriteLine("b4*b5 : " + (b4*b5));
+            Console.WriteLine("b4/b5 : " + (b4/b5));
+            Console.WriteLine("b4>b5 : " + (b4 > b5));
+            Console.WriteLine("b4==b5 : " + (b4 == b5));
+            Console.WriteLine("b4!=b5 : " + (b4 != b5 ));
+            Console.WriteLine("b6>b4 && b6>b5 : " + (b6 > b4 && b6 > b5));
+            Console.WriteLine("b6>b4 && b6==b5 : " + (b6 > b4 && b6 == b5));
+            Console.WriteLine("b6>b4 || b6==b5 : " + (b6 > b4 || b6 == b5));
+            Console.WriteLine("!(b6>b4 || b6==b5) : " + !(b6 > b4 || b6 == b5));
         }
-
-
     }
 }
