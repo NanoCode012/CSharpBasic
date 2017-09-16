2870a879b647ee60bb1e350b62a1e0b3fcf5f797
NanoCode012 <kevinvong@rocketmail.com>
2017-09-16 14:23:37 +0700

    Added new section on file

---
+++ CSharpBasic_Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasic_Files
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
